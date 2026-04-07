e2abd933237b41da945e9ddc683d76e2deb03651
vrunda-shinde <157783605+vrunda-shinde@users.noreply.github.com>
2026-04-07 21:59:38 +0530

    Add files via upload

---
+++ Frontend/src/assets/assets.js
@@ -0,0 +1,206 @@
+import sari1 from './sari1.jpg'
+import sari2 from './sari2.jpg'
+import sari3 from './sari3.jpg'
+import sari4 from './sari1.jpg'
+import sari5 from './sari4.jpg'
+import sari6 from './sari5.jpg'
+import sari7 from './sari1.jpg'
+import sari8 from './sari1.jpg'
+import sari9 from './sari1.jpg'
+import sari10 from './sari1.jpg'
+import diya3 from './diya3.png'
+import search_icon from './search-icon.png'
+import profile_icon from './profile.png'
+import cart from './cart.png'
+import addcart from './add_cart.png'
+import menu from './menu.png'
+import back from './back.png'
+import rating from './rating.png'
+import nonrating from './nonrating.png'
+import back1 from './back1.png'
+import cancel from './cancel.png'
+import filter from './filter.png'
+import bin_icon from './bin_icon.png'
+import exchange_icon from './exchange.png'
+import support_img from './support.png'
+import quality from './quality_icon.png'
+import amruta from './amruta.jpg'
+export const assets={
+    sari1,
+    amruta,
+    sari2,
+    sari3,
+    sari4,
+    sari5,
+    sari6,
+    sari7,
+    sari8,
+    sari9,
+    sari10,
+    diya3,
+    search_icon,
+    profile_icon,
+    cart,
+    addcart,
+    menu,
+    back,
+    rating,
+    nonrating,
+    back1,
+    cancel,
+    filter,
+    bin_icon,
+    exchange_icon,
+    support_img,
+   quality
+
+}
+
+export const products=[
+    {
+        _id:"aaaaa",
+        name:"women sari ",
+        description:"a lightweight usually knitted, pulover shirt ",
+        price:100,
+        image:[sari1,sari2,sari3,sari4],
+        category:"Women",
+        subCategory:"Daily",
+        sizes:["S","M","L"],
+        date:121312313123,
+        bestseller:true    },
+        {
+            _id:"a4aaa",
+            name:"women sari ",
+            description:"a lightweight usually knitted, pulover shirt ",
+            price:100,
+            image:[sari2],
+            category:"Women",
+            subCategory:"Occasional",
+            sizes:["S","M","L"],
+            date:121312313123,
+            bestseller:true    },
+            {
+                _id:"a3aaa",
+                name:"men sari ",
+                description:"a lightweight usually knitted, pulover shirt ",
+                price:300,
+                image:[sari3],
+                category:"Women",
+                subCategory:"Daily",
+                sizes:["S","M","L"],
+                date:121312313123,
+                bestseller:true    },
+                {
+                    _id:"a2aaa",
+                    name:"women sari ",
+                    description:"a lightweight usually knitted, pulover shirt ",
+                    price:200,
+                    image:[sari4],
+                    category:"Women",
+                    subCategory:"Topwear",
+                    sizes:["S","M","L"],
+                    date:121312313123,
+                    bestseller:true    },
+                    {
+                        _id:"a1aaa",
+                        name:"bluish dress",
+                        description:"a lightweight usually knitted, pulover shirt ",
+                        price:100,
+                        image:[sari5],
+                        category:"Kids",
+                        subCategory:"Topwear",
+                        sizes:["S","M","L"],
+                        date:121312313123,
+                        bestseller:true    }
+                        ,
+        {
+            _id:"a4aaa",
+            name:"women sari ",
+            description:"a lightweight usually knitted, pulover shirt ",
+            price:100,
+            image:[sari2],
+            category:"Women",
+            subCategory:"Occasional",
+            sizes:["S","M","L"],
+            date:121312313123,
+            bestseller:true    },
+            {
+                _id:"a3aaa",
+                name:"men sari ",
+                description:"a lightweight usually knitted, pulover shirt ",
+                price:300,
+                image:[sari3],
+                category:"Women",
+                subCategory:"Daily",
+                sizes:["S","M","L"],
+                date:121312313123,
+                bestseller:true    },
+                {
+                    _id:"a2aaa",
+                    name:"women sari ",
+                    description:"a lightweight usually knitted, pulover shirt ",
+                    price:200,
+                    image:[sari4],
+                    category:"Women",
+                    subCategory:"Topwear",
+                    sizes:["S","M","L"],
+                    date:121312313123,
+                    bestseller:true    },
+                    {
+                        _id:"a1aaa",
+                        name:"bluish dress",
+                        description:"a lightweight usually knitted, pulover shirt ",
+                        price:100,
+                        image:[sari5],
+                        category:"Kids",
+                        subCategory:"Topwear",
+                        sizes:["S","M","L"],
+                        date:121312313123,
+                        bestseller:true    }
+                        ,
+        {
+            _id:"a4aaa",
+            name:"women sari ",
+            description:"a lightweight usually knitted, pulover shirt ",
+            price:100,
+            image:[sari2],
+            category:"Women",
+            subCategory:"Occasional",
+            sizes:["S","M","L"],
+            date:121312313123,
+            bestseller:true    },
+            {
+                _id:"a3a1a",
+                name:"men sari ",
+                description:"a lightweight usually knitted, pulover shirt ",
+                price:300,
+                image:[sari3],
+                category:"Women",
+                subCategory:"Daily",
+                sizes:["S","M","L"],
+                date:121312313123,
+                bestseller:true    },
+                {
+                    _id:"a2a2a",
+                    name:"women sari ",
+                    description:"a lightweight usually knitted, pulover shirt ",
+                    price:200,
+                    image:[sari4],
+                    category:"Women",
+                    subCategory:"Topwear",
+                    sizes:["S","M","L"],
+                    date:121312313123,
+                    bestseller:true    },
+                    {
+                        _id:"a13aa",
+                        name:"bluish dress",
+                        description:"a lightweight usually knitted, pulover shirt ",
+                        price:100,
+                        image:[sari5],
+                        category:"Kids",
+                        subCategory:"Topwear",
+                        sizes:["S","M","L"],
+                        date:121312313123,
+                        bestseller:true    }
+
+]
